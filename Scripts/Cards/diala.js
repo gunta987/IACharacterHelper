9e6078d50373881d06422c8b96492ec9bb82c469
Phil Bastian <phil.bastian@smsmt.com>
2016-08-25 19:22:56 +0800

    diala abilities

--- Scripts/Cards/diala.js
+++ Scripts/Cards/diala.js
@@ -11,9 +11,65 @@
                     surges: [[s.stun()], [s.damage()]]
                 },
                 'Cards/Diala/Pic2444795.jpg'),
-            new hf.Ability({ name: 'Force Adept' }, false, 'Cards/Diala/Pic2444785.jpg'),
-            new hf.Ability({ name: 'Force Throw' }, false, 'Cards/Diala/Pic2444786.jpg'),
-            new hf.Ability({ name: 'Battle Meditation' }, false, 'Cards/Diala/Pic2444787.jpg'),
+            new hf.Ability({
+                    name: 'Force Adept',
+                    operations: [
+                        new hf.Operation('Force Adept',
+                            function(hero, conflict) {
+                                conflict.UsedAbilities.push('Force Adept');
+                            },
+                            function(hero, conflict) {
+                                return _.indexOf(conflict.UsedAbilities(), 'Force Adept') === -1;
+                            },
+                            [$.strain()],
+                            C$.ATTACKROLL),
+                        new hf.Operation('Force Adept',
+                            function(hero, conflict) {
+                            },
+                            function(hero, conflict) {
+                                return !hero.activated();
+                            },
+                            [$.strain()],
+                            null,
+                            '(give reroll)')
+                        //TODO: exhaust for fist or spanner ability test
+                    ]
+                },
+                false,
+                'Cards/Diala/Pic2444785.jpg'),
+            new hf.Ability({
+                    name: 'Force Throw',
+                    operations: [
+                        new hf.Operation('Force Throw',
+                            function(hero, conflict, card) {
+                                card.exhausted(true);
+                                hero.testAttribute(hero.eye);
+                            },
+                            function(hero, conflict, card) {
+                                return hero.activated() && !card.exhausted();
+                            },
+                            [$.strain(2)])
+                    ]
+                },
+                false,
+                'Cards/Diala/Pic2444786.jpg'),
+            new hf.Ability({
+                    name: 'Battle Meditation',
+                    events: [
+                        new hf.Event(C$.REST,
+                            function(hero) {
+                                hero.testAttribute(hero.eye,
+                                    function() {
+                                        modal.ConfirmOperation("Battle Meditation: Is the <img src='Tokens/focus.png' /> for you?",
+                                            function() {
+                                                hero.focused(true);
+                                            });
+                                    });
+                            })
+                    ]
+                },
+                false,
+                'Cards/Diala/Pic2444787.jpg'),
             new hf.Ability({
                     name: 'Defensive Stance',
                     events: [
@@ -39,15 +95,34 @@
                 },
                 false,
                 'Cards/Diala/Pic2444789.jpg'),
-            new hf.Ability({ name: 'Snap Kick' }, false, 'Cards/Diala/Pic2444790.jpg'),
+            new hf.Ability({
+                    name: 'Snap Kick',
+                    events: [
+                        new hf.Event(C$.ATTACK_RESOLVED,
+                            function(hero, conflict, card) {
+                                if (!card.exhausted() && !conflict.AttackWeapon().ranged) {
+                                    modal.ConfirmOperation(
+                                        "Do you want to exhaust 'Snap Kick' to roll <img src='Dice/green.png' /> for damage to an adjacent figure?",
+                                        function() {
+                                            card.exhausted(true);
+                                        });
+                                }
+                            })
+                    ]
+                },
+                false,
+                'Cards/Diala/Pic2444790.jpg'),
             new hf.Ability({
                     name: 'Dancing Weapon',
                     operations: [
                         new hf.Operation('Dancing Weapon',
                             function(hero) {
-                                hero.attack([d.BLUE()], [[s.accuracy(2), s.damage()]], true, function(weapon) {
-                                    return !weapon.ranged;
-                                });
+                                hero.attack([d.BLUE()],
+                                    [[s.accuracy(2), s.damage()]],
+                                    true,
+                                    function(weapon) {
+                                        return !weapon.ranged;
+                                    });
                             },
                             function(hero) {
                                 return !hero.stunned() && _(hero.weapons()).some(w => !w.ranged);
@@ -57,7 +132,42 @@
                 },
                 false,
                 'Cards/Diala/Pic2444791.jpg'),
-            new hf.Ability({ name: 'Way of the Sarlacc' }, false, 'Cards/Diala/Pic2444792.jpg'),
+            function() {
+                var activated = false;
+                var attack = function(hero) {
+                    hero.attack(null, null, false, function(weapon) { return !weapon.ranged; });
+                    activated = true;
+                };
+                return new hf.Ability({
+                        name: 'Way of the Sarlacc',
+                        operations: [
+                            new hf.Operation('Way of the Sarlacc',
+                                function(hero) {
+                                    attack(hero);
+                                },
+                                function(hero) {
+                                    return !hero.stunned() && _(hero.weapons()).some(w => !w.ranged);
+                                },
+                                [$.action(), $.strain(2)])
+                        ],
+                        events: [
+                            new hf.Event(C$.ATTACK_RESOLVED,
+                                function(hero) {
+                                    if (activated) {
+                                        modal.AskQuestion('Way of the Sarlacc: are there any more adjacent enemies?',
+                                            function() {
+                                                attack(hero);
+                                            },
+                                            function() {
+                                                activated = false;
+                                            });
+                                    }
+                                })
+                        ]
+                    },
+                    false,
+                    'Cards/Diala/Pic2444792.jpg');
+            }(),
             function() {
                 var foresightActivated = false;
                 return new hf.Weapon({
